77556b792f4a53b6f5160f2b3c3aab2938fba249
Mahdi_AZ <ma.abbaszadeh@mail.sbu.ac.ir>
2019-07-23 13:07:00 +0430

    a new test: one NIOS, two FIFO complete successfully!

---
+++ ThesisProject/QsysComponents/NIOS_with_FIFO/software/NIOS_0/obj/default/hello_world_small.d
@@ -0,0 +1,31 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../NIOS_0_bsp//HAL/inc/sys/alt_stdio.h \
+ ../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+ ../NIOS_0_bsp//HAL/inc/io.h ../NIOS_0_bsp//HAL/inc/alt_types.h \
+ ../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_util.h \
+ ../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_regs.h \
+ ../NIOS_0_bsp/system.h ../NIOS_0_bsp/linker.h \
+ ../NIOS_0_bsp//HAL/inc/sys/alt_irq.h ../NIOS_0_bsp//HAL/inc/nios2.h \
+ ../NIOS_0_bsp//HAL/inc/alt_types.h
+
+../NIOS_0_bsp//HAL/inc/sys/alt_stdio.h:
+
+../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../NIOS_0_bsp//HAL/inc/io.h:
+
+../NIOS_0_bsp//HAL/inc/alt_types.h:
+
+../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_util.h:
+
+../NIOS_0_bsp//drivers/inc/altera_avalon_fifo_regs.h:
+
+../NIOS_0_bsp/system.h:
+
+../NIOS_0_bsp/linker.h:
+
+../NIOS_0_bsp//HAL/inc/sys/alt_irq.h:
+
+../NIOS_0_bsp//HAL/inc/nios2.h:
+
+../NIOS_0_bsp//HAL/inc/alt_types.h:
